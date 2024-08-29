f7378e5f7c3d94e6d1063c3d9e1d127827e26a3b
Alona Lutsenko <alutse@softserveinc.com>
2024-08-29 14:32:25 +0300

    Fixed naming.

--- data_types_homework1/string_slice_homewrok.py
+++ data_types_homework1/string_slice_homewrok.py
@@ -3,6 +3,8 @@ start = int(input("Enter start index: "))
 end = int(input("Enter end index: "))
 
 subStr = text[start:end]
-
 print(subStr)
+
+text2 = "Alona De Lutsenko"
+print(text2[6:9])
 
---
+++ data_types_homework1/sum_numbers_homework.py
@@ -0,0 +1,8 @@
+x = 100
+y = 200
+print(x + y)
+
+# or
+first_number = int(input("Enter first number: "))
+second_number = int(input("Enter second number: "))
+print("sum is: ", first_number + second_number)
